be25776bb107045200d8f1132b6d2e4140ec9653
Daniel Trojanowski <daniel.trojanowski@gmail.com>
2019-08-28 22:14:45 +0200

    Code highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,8 @@
 // custom typefaces
 import "typeface-montserrat"
 import "typeface-merriweather"
+
+// code highlighting
+import "./src/styles/prism.css"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
+import "./src/styles/highlighting.css"
